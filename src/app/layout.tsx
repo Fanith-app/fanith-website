3e579c80403819c93c7d5426646ad475c86d9105
Faisal Khan <fanith.app@gmail.com>
2026-01-20 15:05:26 +0530

    Changes on 20-01-2026 (15:05 - Added Microsoft Clarity Tag)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,16 @@ export default function RootLayout({
             gtag('config', 'G-WDBDHCKBRE');
           `}
         </Script>
+        {/* Microsoft Clarity Tag */}
+        <Script type="text/javascript">
+          {`
+            (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v4c5y9q2hd");
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
